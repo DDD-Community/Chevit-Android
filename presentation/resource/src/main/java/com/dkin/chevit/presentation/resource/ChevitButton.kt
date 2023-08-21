3d74c145022b6fa80e45f1c3ff098b5fd92aefc2
taesung.kim (Stussy) <kts6056@gmail.com>
2023-08-21 22:23:42 +0900

    bugfix: resource build error 수정

--- presentation/resource/src/main/java/com/dkin/chevit/presentation/resource/ChevitButton.kt
+++ presentation/resource/src/main/java/com/dkin/chevit/presentation/resource/ChevitButton.kt
@@ -3,7 +3,9 @@ package com.dkin.chevit.presentation.resource
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
+import android.widget.LinearLayout
 import androidx.annotation.DrawableRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
@@ -13,20 +15,23 @@ class ChevitButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+) : LinearLayout(context, attrs, defStyleAttr) {
     private val binding = ButtonBinding.inflate(LayoutInflater.from(context), this, true)
 
     init {
         val typedArray = attrs?.let {
             context.obtainStyledAttributes(it, R.styleable.ChevitButton)
         }
-        val style = typedArray?.getInt(R.styleable.ChevitButton_buttonStyle, 0) ?: 0
-        val size = typedArray?.getInt(R.styleable.ChevitButton_buttonSize, 0) ?: 0
+        val isEnabled = typedArray?.getBoolean(R.styleable.ChevitButton_isEnabled, true) ?: true
+        val style = typedArray?.getInt(R.styleable.ChevitButton_style, 0) ?: 0
+        val size = typedArray?.getInt(R.styleable.ChevitButton_size, 0) ?: 0
         val drawable = typedArray?.getDrawable(R.styleable.ChevitButton_drawable)
         val text = typedArray?.getString(R.styleable.ChevitButton_text)
         val buttonStyle = Style.valueOf(style)
         val buttonSize = Size.valueOf(size)
 
+        binding.layoutBackground.isClickable = false
+        binding.layoutBackground.isEnabled = isEnabled
         when (buttonStyle) {
             Style.FILL -> R.drawable.bg_button_filled
             Style.OUTLINE -> R.drawable.bg_button_outline
@@ -34,10 +39,11 @@ class ChevitButton @JvmOverloads constructor(
         val fontSize = when (buttonSize) {
             Size.LARGE -> R.dimen.headline_medium
             Size.MEDIUM -> R.dimen.headline_small
-        }.let(context.resources::getDimension)
-        binding.tvTitle.textSize = fontSize
+        }.let(context.resources::getDimensionPixelSize)
+        binding.tvTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize.toFloat())
         setDrawable(drawable)
         setText(text)
+        typedArray?.recycle()
     }
 
     fun setDrawable(@DrawableRes drawableRes: Int?) = with(binding.ivIcon) {
@@ -54,6 +60,10 @@ class ChevitButton @JvmOverloads constructor(
         isVisible = drawable != null
     }
 
+    override fun setOnClickListener(l: OnClickListener?) {
+        binding.layoutBackground.setOnClickListener(l)
+    }
+
     enum class Style {
         FILL, OUTLINE;
 
@@ -63,7 +73,7 @@ class ChevitButton @JvmOverloads constructor(
     }
 
     enum class Size {
-        MEDIUM, LARGE;
+        LARGE, MEDIUM;
 
         companion object {
             fun valueOf(value: Int) = values().find { it.ordinal == value } ?: LARGE
