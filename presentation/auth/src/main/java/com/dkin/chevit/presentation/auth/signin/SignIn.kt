7b2efad1650984c401f63ac2869ed9124bc302c9
taesung.kim (Stussy) <kts6056@gmail.com>
2023-09-04 23:57:18 +0900

    로그인 화면에서 홈 화면으로 랜딩 코드 추가

--- presentation/auth/src/main/java/com/dkin/chevit/presentation/auth/signin/SignIn.kt
+++ presentation/auth/src/main/java/com/dkin/chevit/presentation/auth/signin/SignIn.kt
@@ -2,12 +2,23 @@ package com.dkin.chevit.presentation.auth.signin
 
 import androidx.fragment.app.viewModels
 import com.dkin.chevit.core.mvi.MVIFragment
+import com.dkin.chevit.presentation.auth.R
 import com.dkin.chevit.presentation.auth.databinding.FragmentSignInBinding
+import com.dkin.chevit.presentation.deeplink.DeepLink
+import com.dkin.chevit.presentation.deeplink.deepLink
 
 class SignIn : MVIFragment<FragmentSignInBinding, SignInIntent, SignInState, SignInEffect>(
     FragmentSignInBinding::inflate,
 ) {
     override val viewModel: SignInViewModel by viewModels()
+
+    override fun initView() = binding {
+        layoutGoogle.setOnClickListener {
+            deepLink(DeepLink.Home) {
+                popUpTo(R.id.signIn) { inclusive = true }
+            }
+        }
+    }
 
     override fun processEffect(effect: SignInEffect) {
     }
