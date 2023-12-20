0e45ab0dd8cdbbe684d4f39133f4c13ebee14ca9
SangEun <seun0101@gmail.com>
2023-12-21 00:54:24 +0900

    홈탭 api 병렬처리

--- presentation/home/src/main/java/com/dkin/chevit/presentation/home/HomeViewModel.kt
+++ presentation/home/src/main/java/com/dkin/chevit/presentation/home/HomeViewModel.kt
@@ -2,11 +2,12 @@ package com.dkin.chevit.presentation.home
 
 import androidx.lifecycle.viewModelScope
 import com.dkin.chevit.core.mvi.MVIViewModel
-import com.dkin.chevit.domain.base.get
+import com.dkin.chevit.domain.base.getOrNull
 import com.dkin.chevit.domain.usecase.auth.GetUserUseCase
 import com.dkin.chevit.domain.usecase.plan.GetMyChecklistUseCase
 import com.dkin.chevit.presentation.home.model.CheckListItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,8 +22,7 @@ class HomeViewModel @Inject constructor(
     override suspend fun processIntent(intent: HomeIntent) {
         when (intent) {
             HomeIntent.Initialize -> {
-                getUserInfo()
-                getMyChecklist()
+                getHomeTabInfo()
             }
 
             is HomeIntent.NoticeClicked -> {}
@@ -37,56 +37,42 @@ class HomeViewModel @Inject constructor(
         setEffect { HomeEffect.NavigateToCheckList(id) }
     }
 
-    private fun getUserInfo() {
-        viewModelScope.launch {
-            val user = getUserUseCase(Unit).get()
-            setState {
-                val currentState = state.value
-                if (currentState is HomeState.Stable) {
-                    currentState.copy(
-                        userName = user.name,
-                        profileUrl = user.profileImageUrl,
-                    )
-                } else {
-                    HomeState.Stable(
-                        userName = user.name,
-                        profileUrl = user.profileImageUrl,
-                    )
-                }
-            }
-        }
-    }
-
-    private fun getMyChecklist() {
-        viewModelScope.launch {
-            val checklist = getMyChecklistUseCase(Unit).get()
-            setState {
-                val currentState = state.value
-                if (currentState is HomeState.Stable) {
-                    currentState.copy(
-                        checkList = checklist.list.map {
-                            CheckListItem(
-                                id = it.id,
-                                title = it.schedule.country.name,
-                                date = "${it.schedule.startTime.formatted} ~ ${it.schedule.endTime.formatted}",
-                                isProgress = it.schedule.isProgress,
-                                backgroundUrl = it.schedule.backgroundImageUrl
-                            )
-                        }
-                    )
-                } else {
-                    HomeState.Stable(
-                        checkList = checklist.list.map {
-                            CheckListItem(
-                                id = it.id,
-                                title = it.schedule.country.name,
-                                date = "${it.schedule.startTime.formatted} ~ ${it.schedule.endTime.formatted}",
-                                isProgress = it.schedule.isProgress,
-                                backgroundUrl = it.schedule.backgroundImageUrl
-                            )
-                        }
-                    )
-                }
+    private suspend fun getHomeTabInfo() {
+        val userAsync = async { getUserUseCase(Unit) }
+        val checklistAsync = async { getMyChecklistUseCase(Unit) }
+        listOf(userAsync, checklistAsync).awaitAll()
+        val user = userAsync.await().getOrNull()
+        val checklist = checklistAsync.await().getOrNull()
+        setState {
+            val currentState = state.value
+            if (currentState is HomeState.Stable) {
+                currentState.copy(
+                    userName = user?.name ?: currentState.userName,
+                    profileUrl = user?.profileImageUrl ?: currentState.profileUrl,
+                    checkList = checklist?.list?.map {
+                        CheckListItem(
+                            id = it.id,
+                            title = it.schedule.country.name,
+                            date = "${it.schedule.startTime.formatted} ~ ${it.schedule.endTime.formatted}",
+                            isProgress = it.schedule.isProgress,
+                            backgroundUrl = it.schedule.backgroundImageUrl
+                        )
+                    } ?: listOf()
+                )
+            } else {
+                HomeState.Stable(
+                    userName = user?.name ?: "",
+                    profileUrl = user?.profileImageUrl ?: "",
+                    checkList = checklist?.list?.map {
+                        CheckListItem(
+                            id = it.id,
+                            title = it.schedule.country.name,
+                            date = "${it.schedule.startTime.formatted} ~ ${it.schedule.endTime.formatted}",
+                            isProgress = it.schedule.isProgress,
+                            backgroundUrl = it.schedule.backgroundImageUrl
+                        )
+                    } ?: listOf()
+                )
             }
         }
     }
