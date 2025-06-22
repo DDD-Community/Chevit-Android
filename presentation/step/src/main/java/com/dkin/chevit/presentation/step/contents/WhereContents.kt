536fe8864fc19550b6c0afc1c6b6e4dc4c25be21
taesung.kim (stussy) <kts6056@gmail.com>
2025-06-22 22:16:40 +0900

    빌드 에러 수정

--- presentation/step/src/main/java/com/dkin/chevit/presentation/step/contents/WhereContents.kt
+++ presentation/step/src/main/java/com/dkin/chevit/presentation/step/contents/WhereContents.kt
@@ -15,7 +15,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import com.dkin.chevit.presentation.resource.util.clickableNoRipple
@@ -215,7 +216,7 @@ fun CountryItem(
         modifier
             .clickable(
                 interactionSource = remember { MutableInteractionSource() },
-                indication = rememberRipple(color = ChevitTheme.colors.blue2),
+                indication = null,
             ) { onClick(country) }
             .padding(horizontal = 12.dp, vertical = 18.dp)
     ) {
